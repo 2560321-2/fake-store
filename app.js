a23b86432d5dd44ceb79d5dac03f06c166d4aaba
cristiantrilleras <cris_01lds@yahoo.es>
2023-07-18 21:53:24 -0500

    se corrige codigo js

--- app.js
+++ app.js
@@ -1,57 +1,62 @@
-const img = "https://images.pexels.com/photos/4109850/pexels-photo-4109850.jpeg?auto=compress&cs=tinysrgb&w=600";
+const URL = 'https://fakestoreapi.com/products'
 
-const description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos eveniet unde sunt hic mollitia nihil reprehenderit, vero neque sint saepe debitis quibusdam iusto fugiat illum voluptatum quo repellendus veritatis impedit.";
+const img = "https://images.pexels.com/photos/4109850/pexels-photo-4109850.jpeg?auto=compress&cs=tinysrgb&w=600"
 
-const main = document.querySelector('main');
+const description = `
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos eveniet unde sunt hic mollitia nihil reprehenderit, vero neque sint saepe debitis quibusdam iusto fugiat illum voluptatum quo repellendus veritatis impedit.
+`
+
+const main = document.querySelector('main')
 
 const createCard = () => {
-  const card = document.createElement('div');
-  card.classList.add('card');
+  const card = document.createElement('div')
+  card.classList.add('card')
 
-  const imgCard = document.createElement('img');
-  imgCard.src = img;
-  imgCard.alt = "imagen de producto";
+  
+  const imgCard = document.createElement('img')
+  imgCard.src = img
+  imgCard.alt = "Imagen de producto"
 
-  const titleProduct = document.createElement('h2');
-  titleProduct.textContent = "producto";
+  const titleProduct = document.createElement('h2')
+  titleProduct.textContent = 'Producto'
 
-  const category = document.createElement('p');
-  category.textContent = "category";
+  const categoryCard = document.createElement('p')
+  categoryCard.textContent = 'Category'
 
-  const lorem = document.createElement('p');
-  lorem.textContent = "text";
+  const descriptionCard = document.createElement('p')
+  descriptionCard.textContent = description
 
-  const price = document.createElement('p');
-  price.classList.add('price'); // Use add() method instead of assigning a string directly
+  const priceCard = document.createElement('p')
+  priceCard.classList.add('price')
+  priceCard.textContent = '$price'
 
-  card.appendChild(imgCard);
-  card.appendChild(titleProduct);
-  card.appendChild(category);
-  card.appendChild(lorem);
-  card.appendChild(price);
+  card.appendChild(imgCard)
+  card.appendChild(titleProduct)
+  card.appendChild(categoryCard)
+  card.appendChild(descriptionCard)
+  card.appendChild(priceCard)  
 
-  main.appendChild(card); // Append the card to the main element
+  main.appendChild(card)
+
 }
 
-const URL = 'https://fakestoreapi.com/products?limit=5'
-
-function objects(URL) {
-  return fetch(URL)
-  .then(response => response.json())
-  .then(data => data)
-  .catch(err => console.error(err));  
+function getObjects(URL) {
+    return fetch(URL)
+    .then(response => response.json())
+    .then(data => data)
+    .catch(err => console.error(err));  
 }
 
-const createCards= async ()=>{
-  const datas= await objects(URL)
-  console.log(datas)
-  datas.forEach(product => {
+
+const makingCards = async () => {
+  const products = await getObjects(URL)
+  console.log(products);
+  products.forEach(product => {
     createCard()
-  });
+  });    
 }
 
-window.addEventListener('DOMContentLoaded', createCard)
 
 
 
-
+window.addEventListener('DOMContentLoaded', makingCards)
