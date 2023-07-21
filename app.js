971467d99c81035903814d01d677c1660935a94e
unknown <SENA@ARMDFPCCIFSD974.FormacionSena.RED>
2023-07-21 10:58:26 -0500

    Busqueda de categorias en proceso

--- app.js
+++ app.js
@@ -2,53 +2,93 @@ const img = "https://images.pexels.com/photos/4109850/pexels-photo-4109850.jpeg?
 
 const main = document.querySelector('main')
 
-const createCard = () => {
+const URL = 'https://fakestoreapi.com/products'
+const URL2 = 'https://fakestoreapi.com/products/categories'
+
+const search = document.getElementById('search')
+const finderCategory = document.getElementById('finderCategory')
+
+const createCard = (product) => {
 
   const card = document.createElement('div')
   card.classList = 'card'
 
-  const myImg = document.createElement('img')
-  myImg.src = img
-  myImg.alt = "Imagen del producto"
+  image = document.createElement('img')
+  image.src = product.image
+  image.alt = "Imagen del producto"
 
-  const titleH2 = document.createElement('h2')
-  titleH2.textContent = "Producto"
+  title = document.createElement('h2')
+  title.textContent = product.title
 
-  const textCategory = document.createElement('p')
-  textCategory.textContent = "Category"
+  category = document.createElement('p')
+  category.textContent = product.category
 
-  const textLong = document.createElement('p')
-  textLong.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos eveniet unde sunt hic mollitia nihil reprehenderit, vero neque sint saepe debitis quibusdam iusto fugiat illum voluptatum quo repellendus veritatis impedit."
+  description = document.createElement('p')
+  description.textContent = product.description
 
-  const textPrice = document.createElement('p')
-  textPrice.classList = 'price'
-  textCategory.textContent = "$price"
+  price = document.createElement('p')
+  price.classList = 'price'
+  price.textContent = product.price
 
-  card.appendChild(myImg)
-  card.appendChild(titleH2)
-  card.appendChild(textCategory)
-  card.appendChild(textLong)
-  card.appendChild(textPrice)
+  // {card, myImg, titleH2, textCategory, textLong, textPrice} = info
+
+  card.appendChild(image)
+  card.appendChild(title)
+  card.appendChild(category)
+  card.appendChild(description)
+  card.appendChild(price)
 
   main.appendChild(card)
 }
 
-const URL = 'https://fakestoreapi.com/products'
-
-const data = async (URL) =>{
+function data(URL) {
   return fetch(URL)
-  .then(response => response.json())
-  .then(response => date)
-  .catch(err => console.error(err));
+    .then(response => response.json())
+    .then(data => data)
+    .catch(err => console.error(err));
 }
 
-const createCards = () => {
-  const product = await data(URL)
+const createCards = async () => {
+  const datas = await data(URL)
+  datas.forEach(product => {
+    createCard(product)
+  });
 }
 
-createCards(data)
+/**
+ * Lista de las categorias.
+ */
 
-window.addEventListener('DOMContentLoaded', createCard)
+async function categories(URL2){
+  return await fetch(URL2)
+      .then(res =>res.json())
+      .then(json => console.log(json))
+    }
+
+const searchCategories = () =>{
+  const newCategory = document.createElement('option')
+  const categories2 = categories(URL2)
+  console.log(categories2);
+  // categories2.forEach(element =>{
+  //   console.log(element);
+  // })
+}
+
+searchCategories()
+
+
+    
+
+search.addEventListener('keyup', finderProducts)
+async function finderProducts(event) {
+  const products = await data(URL)
+  const filterProducts = products.filter(product => product.title.includes(event.target.value))
+  main.innerHTML = ''
+  filterProducts.forEach(product => createCard(product));
+
+}
+
+window.addEventListener('DOMContentLoaded', createCards)
 
 
 
